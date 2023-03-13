464d2d4e778edbf9fe43197278cab04cb40d0e87
Henrique Galdino <henrique.galdino@alunos.sc.senac.br>
2023-03-13 16:06:33 -0300

    ..

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function Feed() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Inicio!</Text>
     </View>
   );
@@ -14,7 +14,7 @@ function Feed() {
 
 function Contato() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Contato!</Text>
     </View>
   );
@@ -22,7 +22,7 @@ function Contato() {
 
 function About() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Sobre!</Text>
     </View>
   );
@@ -33,46 +33,51 @@ const Tab = createBottomTabNavigator();
 export default function Apps() {
   return (
     <NavigationContainer>
-    <Tab.Navigator
-      initialRouteName="Inicio"
-      screenOptions={{
-        tabBarActiveTintColor: '#000000',
-      }}
-    >
-      <Tab.Screen
-        name="Inicio"
-        component={Feed}
-        options={{
-          tabBarLabel: 'Inicio',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
+      <Tab.Navigator
+        initialRouteName="Inicio"
+        screenOptions={{
+          tabBarActiveTintColor: "#000000",
         }}
-      />
-      <Tab.Screen
-        name="Sobre"
-        component={About}
-        options={{
-          tabBarLabel: 'Sobre',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="information-outline" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Contato"
-        component={Contato}
-        options={{
-          tabBarLabel: 'Contato',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account-multiple" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+      >
+        <Tab.Screen
+          name="Inicio"
+          component={Feed}
+          options={{
+            tabBarLabel: "Inicio",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Sobre"
+          component={About}
+          options={{
+            tabBarLabel: "Sobre",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="information-outline"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Contato"
+          component={Contato}
+          options={{
+            tabBarLabel: "Contato",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account-multiple"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-
-
